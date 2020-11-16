323eefa7c26cc99b2df7686561d3bc3020d74be3
Kumarjyoti Chakraborty <kumar22in2003@gmail.com>
2020-11-16 23:32:54 +0530

    Removing Junk LUIS Code

--- CoreBotSampleKumar/Dialogs/MainDialog.cs
+++ CoreBotSampleKumar/Dialogs/MainDialog.cs
@@ -97,71 +97,10 @@ namespace CoreBotSampleKumar.Dialogs
                 // LUIS is not configured, we just run the BookingDialog path with an empty BookingDetailsInstance.
                 return await stepContext.BeginDialogAsync(nameof(BookingDialog), new BookingDetails(), cancellationToken);
             }
-
-            // Call LUIS and gather any potential booking details. (Note the TurnContext has the response to the prompt.)
-            //var luisResult = await _luisRecognizer.RecognizeAsync<FlightBooking>(stepContext.Context, cancellationToken);
-            //switch (luisResult.TopIntent().intent)
-            //{
-            //    case FlightBooking.Intent.BookFlight:
-            //        await ShowWarningForUnsupportedCities(stepContext.Context, luisResult, cancellationToken);
-
-            //        // Initialize BookingDetails with any entities we may have found in the response.
-            //        var bookingDetails = new BookingDetails()
-            //        {
-            //            // Get destination and origin from the composite entities arrays.
-            //            Destination = luisResult.ToEntities.Airport,
-            //            Origin = luisResult.FromEntities.Airport,
-            //            TravelDate = luisResult.TravelDate,
-            //        };
-
-            //        // Run the BookingDialog giving it whatever details we have from the LUIS call, it will fill out the remainder.
-            //        return await stepContext.BeginDialogAsync(nameof(BookingDialog), bookingDetails, cancellationToken);
-
-            //    case FlightBooking.Intent.GetWeather:
-            //        // We haven't implemented the GetWeatherDialog so we just display a TODO message.
-            //        var getWeatherMessageText = "TODO: get weather flow here";
-            //        var getWeatherMessage = MessageFactory.Text(getWeatherMessageText, getWeatherMessageText, InputHints.IgnoringInput);
-            //        await stepContext.Context.SendActivityAsync(getWeatherMessage, cancellationToken);
-            //        break;
-
-            //    default:
-            //        // Catch all for unhandled intents
-            //        var didntUnderstandMessageText = $"Sorry, I didn't get that. Please try asking in a different way (intent was {luisResult.TopIntent().intent})";
-            //        var didntUnderstandMessage = MessageFactory.Text(didntUnderstandMessageText, didntUnderstandMessageText, InputHints.IgnoringInput);
-            //        await stepContext.Context.SendActivityAsync(didntUnderstandMessage, cancellationToken);
-            //        break;
-            //}
-
+         
             return await stepContext.NextAsync(null, cancellationToken);
         }
 
-
-        // Shows a warning if the requested From or To cities are recognized as entities but they are not in the Airport entity list.
-        // In some cases LUIS will recognize the From and To composite entities as a valid cities but the From and To Airport values
-        // will be empty if those entity values can't be mapped to a canonical item in the Airport.
-        private static async Task ShowWarningForUnsupportedCities(ITurnContext context, FlightBooking luisResult, CancellationToken cancellationToken)
-        {
-            var unsupportedCities = new List<string>();
-
-            var fromEntities = luisResult.FromEntities;
-            if (!string.IsNullOrEmpty(fromEntities.From) && string.IsNullOrEmpty(fromEntities.Airport))
-            {
-                unsupportedCities.Add(fromEntities.From);
-            }
-
-            var toEntities = luisResult.ToEntities;
-            if (!string.IsNullOrEmpty(toEntities.To) && string.IsNullOrEmpty(toEntities.Airport))
-            {
-                unsupportedCities.Add(toEntities.To);
-            }
-
-            if (unsupportedCities.Any())
-            {
-                var messageText = $"Sorry but the following airports are not supported: {string.Join(',', unsupportedCities)}";
-                var message = MessageFactory.Text(messageText, messageText, InputHints.IgnoringInput);
-                await context.SendActivityAsync(message, cancellationToken);
-            }
-        }
 
         private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
@@ -170,7 +109,6 @@ namespace CoreBotSampleKumar.Dialogs
             if (stepContext.Result is BookingDetails result)
             {
                 // Now we have all the booking details call the booking service.
-
                 // If the call to the booking service was successful tell the user.
 
                 var timeProperty = new TimexProperty(result.TravelDate);
