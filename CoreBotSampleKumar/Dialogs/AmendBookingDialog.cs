f27cacc9781cf8447d91f00ac3f170fe14ea520d
Kumarjyoti Chakraborty <kumar22in2003@gmail.com>
2020-11-17 15:08:19 +0530

    Amend Booking Dialog

---
+++ CoreBotSampleKumar/Dialogs/AmendBookingDialog.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using CoreBotSampleKumar.Bots;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Recognizers.Text.DataTypes.TimexExpression;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.IO;
+
+
+namespace CoreBotSampleKumar.Dialogs
+{
+
+    public class AmendBookingDialog : CancelAndHelpDialog
+    {
+        private const string ConfirmationText = "Your Booking has been successfully cancelled.";
+        private const string RequestBookingIDText = "Enter the BookingID you wish to cancel.";
+
+        public AmendBookingDialog()
+            : base(nameof(AmendBookingDialog))
+        {
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new ConfirmPrompt(nameof(ConfirmPrompt)));
+            AddDialog(new DateResolverDialog());
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+                BookingIDStepAsync,
+                NameStepAsync,
+            }));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> BookingIDStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var bookingDetails = (BookingDetails)stepContext.Options;
+
+            if (bookingDetails.PassengerName == null)
+            {
+                var promptMessage = MessageFactory.Text(RequestBookingIDText, RequestBookingIDText, InputHints.ExpectingInput);
+                return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+            }
+
+            return await stepContext.NextAsync(bookingDetails.PassengerName, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> NameStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var bookingDetails = (BookingDetails)stepContext.Options;
+            bookingDetails.PassengerName = (string)stepContext.Result;
+
+            if (bookingDetails.Destination == null)
+            {
+                var promptMessage = MessageFactory.Text(ConfirmationText, ConfirmationText, InputHints.ExpectingInput);
+                return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+            }
+
+            return await stepContext.NextAsync(bookingDetails.Destination, cancellationToken);
+        }
+
+    }
+}
