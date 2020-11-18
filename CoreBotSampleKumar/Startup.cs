c537e17703bdb20dba5d1fd73d1d1af896416f56
Kumarjyoti Chakraborty <kumar22in2003@gmail.com>
2020-11-18 14:26:46 +0530

    Add App Insights

--- CoreBotSampleKumar/Startup.cs
+++ CoreBotSampleKumar/Startup.cs
@@ -12,11 +12,17 @@ using Microsoft.Extensions.Hosting;
 
 using CoreBotSampleKumar.Bots;
 using CoreBotSampleKumar.Dialogs;
+using Microsoft.Extensions.Configuration;
 
 namespace CoreBotSampleKumar
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -47,6 +53,7 @@ namespace CoreBotSampleKumar
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             services.AddTransient<IBot, DialogAndWelcomeBot<MainDialog>>();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
